7d46502593b92c87b0b054d41e3430eeb8d92b9a
Abrar Ul Haq <abrarulhaq.ios@gmail.com>
2023-08-17 23:09:21 +0500

    Add OutletConnectionsViewControllerTest Suit
    . Add test_zero to confirm communication between Test suit and production

---
+++ OutletConnectionsTests/OutletConnectionsViewControllerTests.swift
@@ -0,0 +1,15 @@
+//
+//  OutletConnectionsViewControllerTests.swift
+//  OutletConnectionsTests
+//
+//  Created by HAQQQABD on 17/08/2023.
+//
+
+import XCTest
+@testable import OutletConnections
+
+final class OutletConnectionsViewControllerTest: XCTestCase {
+    func test_zero(){
+        XCTFail("There is no tests are implemented in OutletConnectionsViewControllerTest suit yet")
+    }
+}
