fbb1c329ab4a29e786f3526a6350178b93b61571
Abrar Ul Haq <abrarulhaq.ios@gmail.com>
2023-08-17 22:56:17 +0500

    Create New Project to Test Outlet connections
    .Overwrite implementation to get control of app launch
    .Add TestingAppDelegate to bypass real delegate during execution of test suit

---
+++ OutletConnections/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  OutletConnections
+//
+//  Created by HAQQQABD on 17/08/2023.
+//
+
+import UIKit
+
+let appDelegate:AnyClass = NSClassFromString("TestingAppDelegate") ?? AppDelegate.self
+UIApplicationMain(CommandLine.argc, CommandLine.unsafeArgv, nil, NSStringFromClass(appDelegate))
---
+++ OutletConnectionsTests/TestingAppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  TestingAppDelegate.swift
+//  OutletConnectionsTests
+//
+//  Created by HAQQQABD on 17/08/2023.
+//
+
+import UIKit
+
+@objc(TestingAppDelegate)
+class TestingAppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        print(">> Launching with testing app delegate")
+        return true
+    }
+}
+
