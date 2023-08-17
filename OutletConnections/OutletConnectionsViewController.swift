5b94e2dd54628caab7716588d8fcf710824ee8e5
Abrar Ul Haq <abrarulhaq.ios@gmail.com>
2023-08-17 22:59:03 +0500

    Add OutletConnectionsViewController

---
+++ OutletConnections/OutletConnectionsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  OutletConnectionsViewController.swift
+//  OutletConnections
+//
+//  Created by HAQQQABD on 17/08/2023.
+//
+
+import UIKit
+
+class OutletConnectionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
